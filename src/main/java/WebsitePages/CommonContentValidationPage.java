67de1432228a83da765a4e3f0f895b476a067222
Neha <nehaarora21192@gmail.com>
2020-12-07 13:15:29 -0500

    RocketAutoValidationTestCasePage

--- src/main/java/WebsitePages/CommonContentValidationPage.java
+++ src/main/java/WebsitePages/CommonContentValidationPage.java
@@ -2,17 +2,20 @@ package WebsitePages;
 
 import Utility.BaseClass;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class CommonContentValidationPage extends BaseClass {
 
     private By rocketAutoImg = By.xpath("//img[contains(@src,'/_nuxt/img/rocket-auto-logo')]");
+    public List<WebElement> headerList = driver.findElements(By.xpath("//ul[@class='nav']/li"));
 
+    public Boolean rocketAutoImageisdisplayed() {
+        return driver.findElement(rocketAutoImg).isDisplayed();
+    }
 
-    public Boolean rocketAutoImageisdisplayed(){
-
-        return  driver.findElement(rocketAutoImg).isDisplayed();
     }
 
 
-}
+
